f345d2d98d9ba0d8214151a3f07f8be4ced8bc2f
Daniel <dkilcullen25@gmail.com>
2021-12-20 14:48:41 -0600

    refactored code

--- src/index.js
+++ src/index.js
@@ -2,41 +2,39 @@
 let form = document.querySelector('#create-task-form')
 let tasks = document.querySelector('#tasks')
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+function renderTask(event) {
   const span = document.createElement('span')
   const li = document.createElement('li')
   li.classList.add('task')
-  const btn = document.createElement('button')
+  
+  const xBtn = document.createElement('button')
+  xBtn.textContent = 'X'
+  xBtn.classList.add('x-btn')
+  xBtn.addEventListener('click', removeTask)
 
   span.appendChild(li)
-  span.appendChild(btn)
+  span.appendChild(xBtn)
   span.style.display = 'flex'
   span.style.justifyContent = 'space-between'
   tasks.appendChild(span)
 
-  const user = document.querySelector('#new-task-user')
-  const priority = document.querySelector('#new-task-priority')
-  li.textContent = `${document.querySelector('#new-task-description').value} - ${user.value} - ${priority.value}`
+  li.textContent = `${event.target['new-task-description'].value} - ${event.target['new-task-user'].value} - ${event.target['new-task-priority'].value}`
   
-  if (priority.value === 'High') {
+  if (event.target['new-task-priority'].value === 'High') {
     li.classList.add('pri-high')
-  } else if (priority.value === 'Medium') {
+  } else if (event.target['new-task-priority'].value === 'Medium') {
     li.classList.add('pri-med')
-  } else if (priority.value === 'Low') {
+  } else if (event.target['new-task-priority'].value === 'Low') {
     li.classList.add('pri-low')
   }
+}
 
-  btn.textContent = 'x'
-  btn.classList.add('x-btn')
-  btn.addEventListener('click', (e) => {
-    span.remove()
-  })
+function removeTask(e) {
+  e.target.parentNode.remove()
+}
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  renderTask(e)
   form.reset()
 })
-
-
-
-
-

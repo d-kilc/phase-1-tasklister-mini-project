1c11e560154025843ef59166a865c93918e79a87
Daniel <dkilcullen25@gmail.com>
2021-12-20 12:01:51 -0600

    added remove task btn; added conditional styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+let form = document.querySelector('#create-task-form')
+let tasks = document.querySelector('#tasks')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const span = document.createElement('span')
+  const li = document.createElement('li')
+  li.classList.add('task')
+  const btn = document.createElement('button')
+
+  span.appendChild(li)
+  span.appendChild(btn)
+  span.style.display = 'flex'
+  span.style.justifyContent = 'space-between'
+  tasks.appendChild(span)
+
+  const user = document.querySelector('#new-task-user')
+  const priority = document.querySelector('#new-task-priority')
+  li.textContent = `${document.querySelector('#new-task-description').value} - ${user.value} - ${priority.value}`
+  
+  if (priority.value === 'High') {
+    li.classList.add('pri-high')
+  } else if (priority.value === 'Medium') {
+    li.classList.add('pri-med')
+  } else if (priority.value === 'Low') {
+    li.classList.add('pri-low')
+  }
+
+  btn.textContent = 'x'
+  btn.classList.add('x-btn')
+  btn.addEventListener('click', (e) => {
+    span.remove()
+  })
+
+  form.reset()
+})
+
+
+
+
+
